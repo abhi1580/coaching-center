7729193067042ea7bfce18889bd687df7b7f6f89
abhi1580 <abhayrpanchal@gmail.com>
2025-04-05 02:29:32 +0530

    minor change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: import.meta.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
