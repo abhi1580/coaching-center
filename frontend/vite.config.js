b58140778dfe9a337ca233f7f88f57defe486e0d
abhi1580 <abhayrpanchal@gmail.com>
2025-04-05 02:43:19 +0530

    chnaged vite.config for render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      host: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL, // ✅ fixed
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
